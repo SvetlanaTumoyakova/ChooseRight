941dc3b69cf97eaadf264a7db749fe8a48d032d4
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-03-13 02:01:11 +0700

    add dialog windows with task for level one

--- app/src/main/java/com/example/chooseright/Level1.java
+++ app/src/main/java/com/example/chooseright/Level1.java
@@ -1,6 +1,10 @@
 package com.example.chooseright;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Level1 extends AppCompatActivity {
 
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.universal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        final ImageView imgLeft = findViewById(R.id.img_left);
+        final ImageView imgRight = findViewById(R.id.img_right);
+
+        imgLeft.setClipToOutline(true);
+        imgRight.setClipToOutline(true);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.preview_dialog);
+        dialog.show();
     }
 }
